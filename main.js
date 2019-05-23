cecdd4727eb3768834c679d14634bfbca681bfd7
Jamil Mbabaali <jamilnineteen91@gmail.com>
2019-05-22 20:33:48 -0500

    play and stop btns function

--- main.js
+++ main.js
@@ -4,22 +4,34 @@ const app = ()=>{
     const outline = document.querySelector('.moving-outline circle');
     const coverArt = document.querySelector('#cover-art');
     const volumeCtrl = document.getElementById('vol-ctrl');
-
+    const outlineLength = outline.getTotalLength();
+    const playBtn=document.getElementById('play');
+    const stopBtn=document.getElementById('stop');
     // Songs
     
-    // Length of outline
-    const outlineLength = outline.getTotalLength();
+    /////////////////Audio Control//////////////////////
     
-    outline.style.strokeDasharray = outlineLength;
-    outline.style.strokeDashoffset= outlineLength;
+    // Buttons
+    playBtn.addEventListener('click',()=>{
+        if (song.paused){
+            song.play();
+            coverArt.style.animation='rotation 4s infinite linear';   
+        }else{
+            song.pause();
+            coverArt.style.animationPlayState ='paused';
+        }
+    });
+
+    stopBtn.addEventListener('click',()=>{
+        song.pause();
+        song.currentTime=0;
+        coverArt.style.animation='none';
+    })
 
     // Volume Control
     volumeCtrl.addEventListener('mousemove',setVolume);
     function setVolume(){
-        console.log(song.volume);
-        console.log(volumeCtrl.value/100);
         song.volume=volumeCtrl.value/100;
-        console.log(song.volume);
     };
 ;
     //Play music
@@ -31,9 +43,14 @@ const app = ()=>{
         }else{
             song.pause();
             coverArt.style.animationPlayState ='paused';
-        }
-        
+        } 
     });
+    
+    /////////////////Seek-bar//////////////////////
+    
+    // Length of outline
+    outline.style.strokeDasharray = outlineLength;
+    outline.style.strokeDashoffset= outlineLength;
     
     song.ontimeupdate = () => {
         // Current song time and duration
@@ -45,7 +62,6 @@ const app = ()=>{
         outline.style.strokeDashoffset=parseInt(progress);
 
         // Cover art animation
-
         if(Math.floor(currentTime)==songDuration){
             coverArt.style.animation='none';
         }
